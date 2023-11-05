2d00c5168132c89111f5921f0a9ff02b4663c53b
deepak <informationtechnology20322@gmail.com>
2023-11-06 01:10:04 +0530

    config url

--- blog_app-fronted/src/config.js
+++ blog_app-fronted/src/config.js
@@ -1,2 +1,2 @@
 // export const baseURL = 'http://127.0.0.1:3001'
-export const baseURL = 'https://blog-app-backend-dun.vercel.app'
+export const baseURL = 'https://blog-app-backend-h6uy.onrender.com'
